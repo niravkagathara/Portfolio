17e87adcd244da0f6914fedea986e39b966d064a
Nirav kagathara <121386601+niravkagathara@users.noreply.github.com>
2024-08-18 13:22:08 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,8 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import healthify from "../../Assets/Projects/healthify.png";
 import readews from "../../Assets/Projects/readews.png";
+import docto from "../../Assets/Projects/1.png";
+import { FaLinkedinIn } from "react-icons/fa";
 
 
 function Projects() {
@@ -18,32 +20,25 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+        <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={healthify}
+              imgPath={docto}
               isBlog={false}
-              title="Olx Clone App"
-              description="the first Flutter OLX clone app! 🚀📱 Successfully developed the basic UI, showcasing my skills in mobile app development. Looking forward to refining and expanding its features.
-              -4 screens 
-              
-              1 splash screen
-              2 home screen
-              3 detail screen
-              4 profile page 
-              
-              create simple UI
-              using
-              list view, grid view, etc.. widget "
-              ghLink="https://www.linkedin.com/posts/nirav-kagathara-80978124b_flutter-appdevelopment-olxclone-activity-7159536736727973888-il1G?utm_source=share&utm_medium=member_desktop"
-              demoLink="https://github.com/niravkagathara/olx_clone_flutter.git"
+              title="Doctor-Appointment Management"
+              description="🚀 Excited to Showcase My Doctor Appointment Management System! 🚀
+I’m thrilled to introduce my latest project, a Doctor Appointment Management System designed to enhance hospital operations. Built with PHP and SQL, this system features a responsive design and offers comprehensive functionalities for patients, doctors, and admins.
+- Responsive Design: Optimized for a seamless experience across all devices.
+How to use site all details show in linkdin post description in 
+"
+ghLink="https://www.linkedin.com/posts/nirav-kagathara-80978124b_webdevelopment-php-sql-activity-7230564598725132288-BlJ8?utm_source=share&utm_medium=member_desktop"
+              demoLink="http://doctor-app.infinityfreeapp.com/DAM"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={readews}
               isBlog={false}
-              title="E-commerce website"
+              title="E-commerce website         "
               description="MERN Stack E-commerce website Features & Technologies:
 
               - Frontend: React, Redux Toolkit, React Router, MUI, Styled Components, Bootstrap
@@ -61,6 +56,26 @@ function Projects() {
               demoLink="https://ecommerce-nk.vercel.app/"
             />
           </Col>
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={healthify}
+              isBlog={false}
+              title="Olx Clone App"
+              description="the first Flutter OLX clone app! 🚀📱 Successfully developed the basic UI, showcasing my skills in mobile app development. Looking forward to refining and expanding its features.
+              -4 screens
+              1 splash screen
+              2 home screen
+              3 detail screen
+              4 profile page 
+              create simple UI
+              using list view, grid view, etc.. widget "
+              ghLink="https://www.linkedin.com/posts/nirav-kagathara-80978124b_flutter-appdevelopment-olxclone-activity-7159536736727973888-il1G?utm_source=share&utm_medium=member_desktop"
+              git="https://github.com/niravkagathara/olx_clone_flutter.git"
+            />
+          </Col>
+
+         
 
         </Row>
       </Container>
