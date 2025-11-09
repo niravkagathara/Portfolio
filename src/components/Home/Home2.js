962a59a330a82af8ca6442472954c85abffde6e6
Nirav kagathara <121386601+niravkagathara@users.noreply.github.com>
2025-11-09 11:44:29 +0530

    Change image format from PNG to JPG

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/Projects/IMG_5439.png";
+import myImg from "../../Assets/Projects/IMG_5439.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
